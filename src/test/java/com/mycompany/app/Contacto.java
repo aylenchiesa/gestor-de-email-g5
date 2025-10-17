d7d599bfe102273f26b0f1ecb37228b107de9b22
Aylen Meza Chiesa <130184706+aylenchiesa@users.noreply.github.com>
2025-10-17 11:21:12 +0000

    contactos

---
+++ src/test/java/com/mycompany/app/Contacto.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ContactoTest {
+  
+  
+  
+  
+  List<Email> noLeidosEnInbox(List<Email> emails)
+  
+  }
