d85504b6cf794b1a248a7cb8875bb72bcee6ce0f
aylen <aylenchiesaok@gmail.com>
2025-10-27 22:44:01 -0300

    //

--- src/test/java/com/mycompany/app/EmailTest.java
+++ src/test/java/com/mycompany/app/EmailTest.java
@@ -2,10 +2,10 @@ package com.mycompany.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
-import java.util.List;
+//import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
